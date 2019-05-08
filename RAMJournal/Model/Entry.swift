c25e4e1a7507c07d93f60a1348a334eed81ff1a4
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2019-05-07 19:20:31 -0600

    Finished inital setup of Entry object in Entry.swift file. Including properties, an initializer, and the equatable protocol stubs

---
+++ RAMJournal/Model/Entry.swift
@@ -0,0 +1,34 @@
+//
+//  Entry.swift
+//  RAMJournal
+//
+//  Created by Taylor Bills on 5/7/19.
+//  Copyright © 2019 Taylor Bills. All rights reserved.
+//
+
+import Foundation
+
+class Entry: Equatable, Codable {
+    
+    
+    // MARK: -  Properties
+    var title: String
+    var body: String
+    var dateSaved: String
+    var entryNumber: Int
+    
+    init(title: String, body: String, dateSaved: String, entryNumber: Int) {
+        self.title = title
+        self.body = body
+        self.dateSaved = dateSaved
+        self.entryNumber = entryNumber
+    }
+    
+    // MARK: -  Equatable Protocol
+    static func == (lhs: Entry, rhs: Entry) -> Bool {
+        return lhs.title == rhs.title &&
+        lhs.body == rhs.body &&
+        lhs.dateSaved == rhs.dateSaved &&
+        lhs.entryNumber == rhs.entryNumber
+    }
+}
